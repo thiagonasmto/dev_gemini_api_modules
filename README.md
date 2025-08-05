b2a5ed46af57e04cdf595829f23ba3db3c7a9c48
thiagonasmto <thiagonasmento20@gmail.com>
2025-08-05 15:16:07 -0300

    Start modules project

---
+++ README.md
@@ -0,0 +1,2 @@
+### Preços
+> O preço da resposta é a soma dos tokens de saída e de pensamento. É possível conferir o número total de tokens de pensamento gerados no campo `thoughtsTokenCount`.
---
+++ main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"google.golang.org/genai"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Erro ao carregar o arquivo .env")
+	}
+
+	apiKey := os.Getenv("GEMINI_API_KEY")
+	if apiKey == "" {
+		log.Fatal("GEMINI_API_KEY não está definido. Defina a chave de API como variável de ambiente.")
+	}
+
+	ctx := context.Background()
+	client, err := genai.NewClient(ctx, &genai.ClientConfig{APIKey: apiKey})
+	if err != nil {
+		log.Fatal(err)
+	}
+	model := "gemini-2.5-flash"
+	prompt := "Explain how AI works in a few words"
+	content := genai.Text(prompt)
+
+	budget := int32(-1) // Define o valor para a configuração do Pensamento do Gemini (0) Desabilita, (0 a 24576 para o modelo flash) Habilita e (-1) Dinâmico.
+	response, err := client.Models.GenerateContent(
+		ctx,
+		model,
+		content,
+		// nil,
+		&genai.GenerateContentConfig{
+			ThinkingConfig: &genai.ThinkingConfig{
+				ThinkingBudget:  &budget, // Habilita, Desabilita ou define como Dinâmico o Pensamento do Gemini
+				IncludeThoughts: false,   // Habilita ou Desabilita o Resumo de Ideias. Resumo de Ideias é a formulação da resposta da LLM.
+			},
+		},
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// for _, part := range response.Candidates[0].Content.Parts {
+	// 	if part.Text != "" {
+	// 		if part.Thought {
+	// 			fmt.Println("Thoughts Summary:")
+	// 			fmt.Println(part.Text)
+	// 		} else {
+	// 			fmt.Println("Answer:")
+	// 			fmt.Println(part.Text)
+	// 		}
+	// 	}
+	// }
+
+	// ...
+	// usageMetadata, err := json.MarshalIndent(response.UsageMetadata, "", "  ")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println("Thoughts tokens:", string(usageMetadata.thoughts_token_count))
+	// fmt.Println("Output tokens:", string(usageMetadata.candidates_token_count))
+
+	// response com streaming
+	// response_stream := client.Models.GenerateContentStream(
+	// 	ctx,
+	// 	model,
+	// 	content,
+	// 	&genai.GenerateContentConfig{
+	// 		ThinkingConfig: &genai.ThinkingConfig{
+	// 			IncludeThoughts: true,
+	// 		},
+	// 	},
+	// )
+
+	// for chunk := range response_stream {
+	// 	for _, part := range chunk.Candidates[0].Content.Parts {
+	// 		if len(part.Text) == 0 {
+	// 			continue
+	// 		}
+
+	// 		if part.Thought {
+	// 			fmt.Printf("Thought: %s\n", part.Text)
+	// 		} else {
+	// 			fmt.Printf("Answer: %s\n", part.Text)
+	// 		}
+	// 	}
+	// }
+
+	fmt.Println(response)
+}
